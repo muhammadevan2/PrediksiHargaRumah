5c170540e245491f19d2e577601a757be1239530
Muhammad Evan <115947084+muhammadevan2@users.noreply.github.com>
2023-07-04 12:12:55 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+#load dataset
+df = pd.read_excel("/content/DATA RUMAH.xlsx")
+
+#train model
+x = data[["LB","LT","KT","KM","GRS"]]
+y = data[["HARGA"]]
+
+#streamlit app
+st.title("Prediksi Harga Rumah")
+st.write('Masukan Rincian Rumah yang diinginkan')
+
+#inputan
+luas_bangunan = int(input("Masukkan Luas Tanah: "))
+luas_tanah = int(input("Masukkan Luas Bangunan: "))
+unit_KM = int(input("Masukkan Banyak Kamar Mandi: "))
+unit_KT = int(input("Masukkan Banyak Kamar Tidur: "))
+unit_GRS = int(input("Masukkan Banyak Garasi: "))
+
+#tombol prediksi
+if st.button('Cari')
+    prediksi = model.predict(([[luas_bangunan, luas_tanah, unit_KM, unit_KT, unit_GRS]]))
+    st.write("Harga Prediksi Rumah Sebesar Rp" + harga_format)
+    harga_format = "{:,.0f}".format(harga).replace(",", ".")
